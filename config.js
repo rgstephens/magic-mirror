ddba9e44d1b6b78fcf0bc8232e75a19376c22744
Greg Stephens <greg@udon.org>
2017-12-28 13:47:49 -0800

    Remove alert & updateNotifications

--- config.js
+++ config.js
@@ -44,13 +44,6 @@ var config = {
       }
     },
     {
-      module: "alert"
-    },
-    {
-      module: "updatenotification",
-      position: "top_bar"
-    },
-    {
       module: "clock",
       position: "top_left"
     },
